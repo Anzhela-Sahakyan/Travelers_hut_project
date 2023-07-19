f904fe01333adbed379ec0303472cd70c5de9dd2
Anzhela <sahakyananzhela@gmail.com>
2023-07-20 03:22:31 +0400

    feat: adding Menu component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
+import Menu from "./Components/Menu/Menu";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Home />
       <About />
+      <Menu />
     </div>
   );
 }
