a7f8e5285351672b82af19e806020641c59feb59
Anzhela <sahakyananzhela@gmail.com>
2023-07-25 15:27:00 +0400

    feat(Data->Menu): adding Salads.tsx

---
+++ src/Data/Menu/salads.tsx
@@ -0,0 +1,104 @@
+const Salads = [
+  {
+    name: "Tabbouleh",
+    price: "1000 AMD",
+    ingredients: ["tomato", "lettuce", "bulgur"],
+  },
+  {
+    name: "Greek Salad",
+    price: "1200 AMD",
+    ingredients: [
+      "cucumber",
+      "tomato",
+      "red onion",
+      "kalamata olives",
+      "feta cheese",
+    ],
+  },
+  {
+    name: "Caesar Salad",
+    price: "1500 AMD",
+    ingredients: [
+      "romaine lettuce",
+      "croutons",
+      "Parmesan cheese",
+      "Caesar dressing",
+    ],
+  },
+  {
+    name: "Cobb Salad",
+    price: "1800 AMD",
+    ingredients: [
+      "chicken breast",
+      "avocado",
+      "bacon",
+      "tomato",
+      "hard-boiled egg",
+      "blue cheese",
+    ],
+  },
+  {
+    name: "Nicoise Salad",
+    price: "1700 AMD",
+    ingredients: [
+      "tuna",
+      "green beans",
+      "potatoes",
+      "olives",
+      "tomato",
+      "hard-boiled egg",
+    ],
+  },
+  {
+    name: "Waldorf Salad",
+    price: "1300 AMD",
+    ingredients: ["apple", "celery", "walnuts", "grapes", "mayonnaise"],
+  },
+  {
+    name: "Caprese Salad",
+    price: "1400 AMD",
+    ingredients: [
+      "mozzarella cheese",
+      "tomato",
+      "fresh basil leaves",
+      "balsamic glaze",
+    ],
+  },
+  {
+    name: "Fattoush Salad",
+    price: "1100 AMD",
+    ingredients: [
+      "cucumber",
+      "tomato",
+      "green bell pepper",
+      "radish",
+      "sumac",
+      "pita chips",
+    ],
+  },
+  {
+    name: "Couscous Salad",
+    price: "1600 AMD",
+    ingredients: [
+      "couscous",
+      "cherry tomatoes",
+      "cucumber",
+      "red onion",
+      "mint",
+      "lemon dressing",
+    ],
+  },
+  {
+    name: "Spinach and Strawberry Salad",
+    price: "1900 AMD",
+    ingredients: [
+      "baby spinach",
+      "strawberries",
+      "goat cheese",
+      "candied pecans",
+      "balsamic vinaigrette",
+    ],
+  },
+];
+
+export default Salads;
